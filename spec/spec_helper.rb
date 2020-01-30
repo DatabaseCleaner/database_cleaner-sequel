30c4a34f501e432433f817cca6d0119460a161f9
Micah Geisel <micah@botandrose.com>
2020-01-30 07:49:39 -0800

    chop down repo to just database_cleaner-sequel adapter.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,14 @@
 require "bundler/setup"
-require 'database_cleaner'
+require "database_cleaner/sequel"
 
 RSpec.configure do |config|
-  # These two settings work together to allow you to limit a spec run
-  # to individual examples or groups you care about by tagging them with
-  # `:focus` metadata. When nothing is tagged with `:focus`, all examples
-  # get run.
-  config.filter_run :focus
-  config.run_all_when_everything_filtered = true
+  # Enable flags like --only-failures and --next-failure
+  config.example_status_persistence_file_path = ".rspec_status"
 
+  # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
-end
 
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
