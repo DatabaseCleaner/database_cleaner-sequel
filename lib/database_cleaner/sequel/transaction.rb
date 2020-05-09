0ec3be3256f57520f671a239c653fcee9eb18475
Micah Geisel <micah@botandrose.com>
2020-05-09 12:08:00 -0700

    this jruby bug was fixed back in 2013, so we can remove this workaround. see https://github.com/DatabaseCleaner/database_cleaner/issues/256

--- lib/database_cleaner/sequel/transaction.rb
+++ lib/database_cleaner/sequel/transaction.rb
@@ -3,18 +3,7 @@ require 'database_cleaner/sequel/base'
 module DatabaseCleaner
   module Sequel
     class Transaction < Base
-      def self.check_fiber_brokenness
-        if !@checked_fiber_brokenness && Fiber.new { Thread.current }.resume != Thread.current
-          raise RuntimeError, "This ruby engine's Fibers are not compatible with Sequel's connection pool. " +
-            "To work around this, please use DatabaseCleaner.cleaning with a block instead of " +
-            "DatabaseCleaner.start and DatabaseCleaner.clean"
-        end
-        @checked_fiber_brokenness = true
-      end
-
       def start
-        self.class.check_fiber_brokenness
-
         @fibers ||= []
         db = self.db
         f = Fiber.new do
@@ -29,10 +18,6 @@ module DatabaseCleaner
       def clean
         f = @fibers.pop
         f.resume
-      end
-
-      def cleaning
-        self.db.transaction(:rollback => :always, :savepoint => true, :auto_savepoint => true) { yield }
       end
     end
   end
